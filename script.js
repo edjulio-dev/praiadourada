f2c2957da0c6034ffec16d03ee5d200050f992cb
edjulio-dev <eduardoj.eletrica@gmail.com>
2026-03-07 22:20:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,61 @@ alert("Por favor, assine o checklist.");
 return;
 }
 
-alert("Checklist enviado com sucesso!");
+function gerarPDF(){
+
+if(signaturePad.isEmpty()){
+alert("Por favor, assine o checklist.");
+return;
+}
+
+const element = document.getElementById("checklistForm");
+
+const nome = document.querySelector("input[type='text']").value;
+
+const opt = {
+
+margin:0.5,
+filename:`checklist-flat-${nome}.pdf`,
+
+image:{ type:'jpeg', quality:0.98 },
+
+html2canvas:{
+scale:2
+},
+
+jsPDF:{
+unit:'in',
+format:'a4',
+orientation:'portrait'
+}
+
+};
+
+html2pdf().set(opt).from(element).save();
+
+}
+
+function abrirWhatsapp(){
+
+const telefone = document.getElementById("whatsapp").value;
+
+if(telefone.length < 10){
+alert("Digite um número de WhatsApp válido");
+return;
+}
+
+// adiciona código do Brasil se não tiver
+if (!telefone.startsWith("55")) {
+    telefone = "55" + telefone;
+}
+
+const mensagem = "Olá! Segue o checklist do seu flat.";
+
+const url = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem);
+
+window.open(url, "_blank");
+
+}
 
 });
+
