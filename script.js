34a8786f2aa8cf55ff344b78e4088fd83b30ede6
edjulio-dev <eduardoj.eletrica@gmail.com>
2026-03-07 23:33:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const canvas = document.getElementById("signaturePad");
 
-const signaturePad = new SignaturePad(canvas,{
-backgroundColor:"rgba(255,255,255,1)"
+const signaturePad = new SignaturePad(canvas, {
+  backgroundColor: "rgba(255,255,255,1)"
 });
 
-function resizeCanvas(){
+function resizeCanvas() {
+  const ratio = Math.max(window.devicePixelRatio || 1, 1);
 
-const ratio = Math.max(window.devicePixelRatio || 1,1);
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
 
-canvas.width = canvas.offsetWidth * ratio;
-canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
 
-canvas.getContext("2d").scale(ratio,ratio);
-
-signaturePad.clear();
+  signaturePad.clear();
 }
 
-window.addEventListener("resize",resizeCanvas);
+window.addEventListener("resize", resizeCanvas);
 resizeCanvas();
 
-function clearSignature(){
-signaturePad.clear();
+function clearSignature() {
+  signaturePad.clear();
 }
 
-function allowOnlyNumbers(input){
-
-input.addEventListener("input",function(){
-this.value = this.value.replace(/\D/g,'');
-});
-
+function allowOnlyNumbers(input) {
+  input.addEventListener("input", function () {
+    this.value = this.value.replace(/\D/g, '');
+  });
 }
 
 allowOnlyNumbers(document.getElementById("cpf"));
@@ -37,23 +34,22 @@ allowOnlyNumbers(document.getElementById("whatsapp"));
 
 // -------- WHATSAPP --------
 
-function abrirWhatsapp(){
+function abrirWhatsapp() {
 
-let telefone = document.getElementById("whatsapp").value;
+  let telefone = document.getElementById("whatsapp").value;
 
-if(telefone.length < 10){
-alert("Digite um número de WhatsApp válido");
-return;
-}
+  if (telefone.length < 10) {
+    alert("Digite um número de WhatsApp válido");
+    return;
+  }
 
-if (!telefone.startsWith("55")) {
-telefone = "55" + telefone;
-}
+  if (!telefone.startsWith("55")) {
+    telefone = "55" + telefone;
+  }
 
-const mensagem = "Olá! Segue o checklist do seu flat.";
+  const mensagem = "Olá! Segue o checklist do seu flat.";
 
-const url = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem);
+  const url = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem);
 
-window.open(url, "_blank");
-
+  window.open(url, "_blank");
 }
