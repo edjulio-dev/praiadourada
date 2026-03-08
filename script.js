f876b94552dc17043fb270eb9430795d271ce44d
edjulio-dev <eduardoj.eletrica@gmail.com>
2026-03-07 23:17:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,71 +33,7 @@ this.value = this.value.replace(/\D/g,'');
 
 allowOnlyNumbers(document.getElementById("cpf"));
 allowOnlyNumbers(document.getElementById("whatsapp"));
-
-
-// -------- GERAR PDF --------
-
-async function gerarPDF(){
-
-if(signaturePad.isEmpty()){
-alert("Por favor, assine o checklist.");
-return;
-}
-
-const element = document.getElementById("checklistForm");
-
-const nome = document.querySelector("input[type='text']").value || "checklist";
-
-const opt = {
-
-margin:10,
-
-filename:`checklist-flat-${nome}.pdf`,
-
-image:{ type:'jpeg', quality:0.98 },
-
-html2canvas:{
-scale:2,
-useCORS:true
-},
-
-jsPDF:{
-unit:'mm',
-format:'a4',
-orientation:'portrait'
-},
-
-pagebreak:{
-mode:['avoid-all','css','legacy']
-}
-
-};
-
-const worker = html2pdf().set(opt).from(element);
-
-const pdfBlob = await worker.outputPdf('blob');
-
-const file = new File([pdfBlob], `checklist-flat-${nome}.pdf`, {type:'application/pdf'});
-
-if(navigator.share && navigator.canShare({files:[file]})){
-
-await navigator.share({
-title:"Checklist",
-text:"Checklist",
-files:[file]
-});
-
-}else{
-
-// fallback para desktop
-const url = URL.createObjectURL(pdfBlob);
-const link = document.createElement("a");
-link.href = url;
-link.download = `checklist-flat-${nome}.pdf`;
-link.click();
-
-}
-
+allowOnlyNumbers(document.getElementById("flat"));
 }
 
 
@@ -123,4 +59,5 @@ const url = "https://wa.me/" + telefone + "?text=" + encodeURIComponent(mensagem
 window.open(url, "_blank");
 
 }
+
 
